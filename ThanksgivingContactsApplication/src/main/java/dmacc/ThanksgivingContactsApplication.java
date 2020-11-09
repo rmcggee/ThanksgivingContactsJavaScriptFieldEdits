12f616315dac8fd6bd27b7d2857a3fa1643116ea
rmcggee <ramcgee1@dmacc.edu>
2020-11-08 20:35:12 -0600

    Upload project with java script edits

---
+++ ThanksgivingContactsApplication/src/main/java/dmacc/ThanksgivingContactsApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanksgivingContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanksgivingContactsApplication.class, args);
+	}
+
+}
